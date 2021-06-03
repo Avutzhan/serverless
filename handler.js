8b711f0e893f2d4379a06bedb25cfd8001bb917b
Avutzhan Dautov <dautov92@list.ru>
2021-06-03 11:58:47 +0600

    Deploying A Function 2

--- handler.js
+++ handler.js
@@ -1,12 +1,19 @@
 'use strict';
 
+const emojis = [
+    '😀', '😆', '🤣', '🤩', '🤑', '🚀'
+];
+
 module.exports.rank = async (event) => {
+  const rank = event.queryStringParameters.rank;
+  const rankEmoji = emojis[rank > emojis.length ? emojis.length - 1 : rank];
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        input: rankEmoji,
       },
       null,
       2
